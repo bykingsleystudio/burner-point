c664ee2ea5161f8be067e3fcd3690f519f64129c
Ndukwe Kingsley Uche <ndukwekingsleyuche@gmail.com>
2026-08-16 01:38:15 +0100

    dockerfile update

--- apps/api/src/modules/api-platform/api-platform.module.ts
+++ apps/api/src/modules/api-platform/api-platform.module.ts
@@ -4,9 +4,13 @@ import { ApiPlatformController } from './api-platform.controller';
 import { ApiPlatformService } from './api-platform.service';
 import { ApiKey, DeveloperWebhook, DeveloperWebhookDelivery } from '../../database/entities/extended-entities';
 import { ApiKeyOrJwtGuard } from './api-key.guard';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ApiKey, DeveloperWebhook, DeveloperWebhookDelivery])],
+  imports: [
+    TypeOrmModule.forFeature([ApiKey, DeveloperWebhook, DeveloperWebhookDelivery]),
+    AuthModule,
+  ],
   controllers: [ApiPlatformController],
   providers: [ApiPlatformService, ApiKeyOrJwtGuard],
   exports: [ApiPlatformService, ApiKeyOrJwtGuard],
